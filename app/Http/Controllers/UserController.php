80522aa683e24d0f354ae6e0b624bb9c9f7f1fac
Leonardo Akio <akioleonardo@gmail.com>
2021-09-24 12:24:39 -0300

    CRIAÇÃO DE USUÁRIO NO CONTROLLER E RETORNO NA VIEW

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use app\User;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function ListUser()
     {
-        $user = new User();
-        echo "Listagem de usuario";
+        // $user = new User();
+        // $user -> name = 'Leonardo Akio';
+        // $user -> email = 'teste@teste.com.br';
+        // $user -> password = Hash::make('password');
+        // $user -> save();
+        $user = User::where('id', '=', 1)->first();
+        return view ('listUser', [
+            'userList' => $user
+
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('listagem-usuario', 'UserController@ListUser');
+Route::get('usuarios', 'Form\\TestController@listAllUsers')->name('users.listAll');
